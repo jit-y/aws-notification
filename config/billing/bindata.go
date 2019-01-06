e486f2fd215bd88c4ee1b9bfe3bd395449a6b5da
YUJI takahashi <jit.y.31@gmail.com>
2019-01-07 01:33:03 +0900

    Add servicename

--- config/billing/bindata.go
+++ config/billing/bindata.go
@@ -6,11 +6,16 @@ import (
 	"github.com/jessevdk/go-assets"
 )
 
-var _Assetsda35f53e42a2c065d1370dd5e00e6dfd1a288c63 = "- AmazonEC2\n- AmazonRoute53\n- AmazonECR\n- AWSLambda\n- AmazonS3\n- AmazonCloudFront\n"
+var _Assetsda35f53e42a2c065d1370dd5e00e6dfd1a288c63 = "- AmazonEC2\n- AmazonRoute53\n- AmazonECR\n- AWSLambda\n- AmazonS3\n- AmazonCloudFront\n- AmazonLightSail\n"
 
 // Assets returns go-assets FileSystem
-var Assets = assets.NewFileSystem(map[string][]string{"/billing": []string{"servicename.yml"}, "/": []string{"billing"}}, map[string]*assets.File{
-	"/billing": &assets.File{
+var Assets = assets.NewFileSystem(map[string][]string{"/": []string{"billing"}, "/billing": []string{"servicename.yml"}}, map[string]*assets.File{
+	"/": &assets.File{
+		Path:     "/",
+		FileMode: 0x800001ed,
+		Mtime:    time.Unix(1546791225, 1546791225000000000),
+		Data:     nil,
+	}, "/billing": &assets.File{
 		Path:     "/billing",
 		FileMode: 0x800001ed,
 		Mtime:    time.Unix(1546788996, 1546788996000000000),
@@ -18,11 +23,6 @@ var Assets = assets.NewFileSystem(map[string][]string{"/billing": []string{"serv
 	}, "/billing/servicename.yml": &assets.File{
 		Path:     "/billing/servicename.yml",
 		FileMode: 0x1a4,
-		Mtime:    time.Unix(1546788996, 1546788996000000000),
+		Mtime:    time.Unix(1546791588, 1546791588000000000),
 		Data:     []byte(_Assetsda35f53e42a2c065d1370dd5e00e6dfd1a288c63),
-	}, "/": &assets.File{
-		Path:     "/",
-		FileMode: 0x800001ed,
-		Mtime:    time.Unix(1546791072, 1546791072000000000),
-		Data:     nil,
 	}}, "")
